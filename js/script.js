f6773e24a05930ac5b7f34a73fdafdca82ce47bb
Joseph Yhu <xbhlx7@gmail.com>
2020-03-14 19:34:20 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,115 +1,38 @@
 let red;
 let green;
 let blue;
+class Quote {
+  constructor(quote, source, type, citation, year) {
+    this.quote = quote;
+    this.source = source;
+    this.type = type;
+    this.citation = citation;
+    this.year = year;
+  }
+}
 const quotes = [
-  {quote: "Frankly, my dear, I don't give a damn.",
-   source: "Rhett Butler",
-   type: "movie",
-   citation: "Gone with the Wind",
-   year: 1939
- },
- {quote: "I'm gonna make him an offer he can't refuse.",
-  source: "Vito Corleone",
-  type: "movie",
-  citation: "The Godfather",
-  year: 1972
- },
- {quote: "You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
-  source: "Terry Malloy",
-  type: "movie",
-  citation: "On the Waterfront",
-  year: 1954
- },
- {quote: "Toto, I've a feeling we're not in Kansas anymore.",
-  source: "Dorothy Gale",
-  type: "movie",
-  citation: "The Wizard of Oz",
-  year: 1939
- },
- {quote: "Here's looking at you, kid.",
-  source: "Rick Blaine",
-  type: "movie",
-  citation: "Casablanca",
-  year: 1942
- },
- {quote: "All right, Mr. DeMille, I'm ready for my close-up.",
-  source: "Norma Desmond",
-  type: "movie",
-  citation: "Sunset Boulevard",
-  year: 1950
- },
- {quote: "May the Force be with you.",
-  source: "Han Solo",
-  type: "movie",
-  citation: "Star Wars",
-  year: 1977
- },
- {quote: "Fasten your seatbelts. It's going to be a bumpy night.",
-  source: "Margo Channing",
-  type: "movie",
-  citation: "All About Eve",
-  year: 1950
- },
- {quote: "You talkin' to me?",
-  source: "Travis Bickle",
-  type: "movie",
-  citation: "Taxi Driver",
-  year: 1976
- },
- {quote: "El Psy Kongroo.",
-  source: "Okabe Rintaro",
-  type: "visual novel",
-  citation: "Steins;Gate",
-  year: 2009
- },
- {quote: "Deceive the first you. Deceive the world. That is the choice to arrive at the 'Steins Gate.'",
-  source: "Okabe Rintaro",
-  type: "visual novel",
-  citation: "Steins;Gate",
-  year: 2009
- },
- {quote: "I am mad scientist. It's so cool. Son of a bitch.",
-  source: "Okabe Rintaro",
-  type: "anime",
-  citation: "Steins;Gate",
-  year: 2011
- },
- {quote: "Meruem, King of Ants, you know nothing...of humanity's infinite potential for malice!",
-  source: "Isaac Netero",
-  type: "anime",
-  citation: "Hunter x Hunter (2011)",
-  year: 2014
- },
- {quote: "Explosion!",
-  source: "Megumin",
-  type: "anime",
-  citation: "KonoSuba: God's Blessing on This Wonderful World!",
-  year: 2016
- },
- {quote: "People die if they are killed...",
-  source: "Emiya Shirou",
-  type: "anime",
-  citaton: "Fate/stay night",
-  year: 2006
- },
- {quote: "WAR IS PEACE, FREEDOM IS SLAVERY, IGNORANCE IS STRENGTH.",
-  source: "George Orwell",
-  type: "book",
-  citation: "Nineteen Eighty-Four",
-  year: 1949
- },
- {quote: "All animals are equal, but some animals are more equal than others.",
-  source: "George Orwell",
-  type: "book",
-  citation: "Animal Farm",
-  year: 1945
- },
- {quote: "One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them.",
-  source: "J. R. R. Tolkien",
-  type: "book",
-  citation: "The Lord of the Rings",
-  year: 1954
- }
+  new Quote("Frankly, my dear, I don't give a damn.", "Rhett Butler", "movie", "Gone with the Wind", 1939),
+  new Quote("I'm gonna make him an offer her can't refuse.", "Vito Corleone", "movie", "The Godfather", 1972),
+  new Quote("You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
+            "Terry Malloy", "movie", "On the Waterfront", 1954),
+  new Quote("Toto, I've a feeling we're not in Kanas anymore.", "Dorothy Gale", "movie", "The Wizard of Oz", 1939),
+  new Quote("Here's looking at you, kid.", "Rick Blaine", "movie", "Casablanca", 1942),
+  new Quote("All right, Mr . DeMille, I'm ready for my close-up.", "Norma Desmond", "movie", "Sunset Boulevard", 1950),
+  new Quote("May the Force be with you.", "Han Solo", "movie", "Star Wars", 1977),
+  new Quote("Fasten your seatbelts. It's going to be a bumpy night.", "Margo Channing", "movie", "All About Eve", 1950),
+  new Quote("You talkin' to me?", "Travis Bickle", "movie", "Taxi Driver", 1976),
+  new Quote("El Psy Kongroo.", "Okabe Rintaro", "visual novel", "Steins;Gate", 2009),
+  new Quote("Deceive the first you. Deceive the world. That is the choice to arrive at the 'Steins Gate.'",
+            "Okabe Rintaro", "visual novel", "Steins;Gate", 2009),
+  new Quote("I am mad scientist. It's so cool. Son of a bitch.", "Okabe Rintaro", "anime", "Steins;Gate", 2011),
+  new Quote("Meruem, King of Ants, you know nothing...of humanity's infinite potential for malice!",
+            "Isaac Netero", "anime", "Hunter x Hunter (2011)", 2014),
+  new Quote("Explosion!", "Megumin", "anime", "KonoSuba: God's Blessing on This Wonderful World!", 2016),
+  new Quote("People die if they are killed...", "Emiya Shirou", "anime", "Fate/stay night", 2006),
+  new Quote("WAR IS PEACE. FREEDOM IS SLAVERY. IGNORANCE IS STRENGTH", "George Orwell", "book", "Nineteen Eighty-Four", 1949),
+  new Quote("All animals are equal, but some animals are more equal than others.", "George Orwell", "book", "Animal Farm", 1945),
+  new Quote("One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them.",
+            "J. R. R. Tolkien", "book", "The Lord of the Rings", 1954)
 ];
 
 const getRandomQuote = array => array[Math.floor(Math.random() * quotes.length)];
